2fd8758ee107eabe84579e64ea65eac10da42da9
Maximilian Mauroner <github@mauroner.eu>
2022-11-05 20:56:05 +0100

    added error msg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ type singleStudyDate = RouterTypes['studyDate']['getAll']['output'][0]
 const Home: NextPage = () => {
         const [userData, setUserData] = useState<studyDate>([])
         const [isLoadingMutation, setIsLoadingMutation] = useState(false);
-        const {data} = trpc.studyDate.getAll.useQuery();
+        const [isErrorMutation, setIsErrorMutation] = useState(false);
         const {isLoading} = trpc.studyDate.getAll.useQuery(undefined, {
             onSuccess: (data) => {
                 setUserData(data)
@@ -21,41 +21,42 @@ const Home: NextPage = () => {
         return (
             <>
                 <Head>
-                    <title>Study Helper</title>
-                    <meta name="description" content="Study Helper by Maximilian"/>
+                    <title>Study Group</title>
+                    <meta name="description" content="Study Group by Maximilian"/>
                     <link rel="icon" href="/favicon.ico"/>
                 </Head>
                 <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+                    {isErrorMutation ? <ErrorCard setError={setIsErrorMutation}/> : null}
                     <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
                         Select your Day
                     </h1>
 
                     <div className="my-3 grid gap-4 pt-3 text-center grid-cols-2 lg:w-2/3 w-full">
                         <DayCard name={"Saturday"} description={"Saturday at 12:00"} dateEnum={"SATURDAY"}
-                                 setData={setUserData} setLoading={setIsLoadingMutation}/>
+                                 setData={setUserData} setLoading={setIsLoadingMutation} setError={setIsErrorMutation}/>
                         <DayCard name={"Sunday"} description={"Sunday at 12:00"} dateEnum={"SUNDAY"}
-                                 setData={setUserData} setLoading={setIsLoadingMutation}/>
+                                 setData={setUserData} setLoading={setIsLoadingMutation} setError={setIsErrorMutation}/>
                     </div>
                     <div className="my-3 grid gap-4 pt-3 text-center grid-cols-2 lg:w-2/3 w-full">
                         {isLoading || isLoadingMutation ? <> <LoadingComponent/> <LoadingComponent/>  </> : (<>
                             <div>
                                 {userData?.map((studyDate: singleStudyDate) => (
-                                    <>
+                                    <div key={studyDate.id}>
                                         {studyDate.date === "SATURDAY" && studyDate.user.name && studyDate.user.image ?
-                                            <UserCard key={studyDate.id} name={studyDate.user.name}
+                                            <UserCard name={studyDate.user.name}
                                                       image={studyDate.user.image}
                                                       studyDate={"SATURDAY"}/> : null}
-                                    </>
+                                    </div>
                                 ))}
                             </div>
                             <div>
                                 {userData?.map((studyDate: singleStudyDate) => (
-                                    <>
+                                    <div key={studyDate.id}>
                                         {studyDate.date === "SUNDAY" && studyDate.user.name && studyDate.user.image ?
                                             <UserCard key={studyDate.id} name={studyDate.user.name}
                                                       image={studyDate.user.image}
                                                       studyDate={"SUNDAY"}/> : null}
-                                    </>
+                                    </div>
                                 ))}
                             </div>
                         </>)}
@@ -76,12 +77,13 @@ type DayCardProps =
         dateEnum: string;
         setData: (data: studyDate) => void;
         setLoading: (isLoading: boolean) => void;
+        setError: (isError: boolean) => void;
     }
     ;
 
 const DayCard = (
         {
-            name, description, dateEnum, setData, setLoading
+            name, description, dateEnum, setData, setLoading, setError
         }
             : DayCardProps) => {
         const mutation = trpc.studyDate.selectDay.useMutation();
@@ -92,28 +94,29 @@ const DayCard = (
                     setData(data)
                     setLoading(false)
                 },
+                onError: () => {
+                    setError(true)
+                    setLoading(false)
+                }
             });
         }
         const borderColor = dateEnum === "SATURDAY" ? "border-purple-500" : "border-indigo-500";
         return (
-            <button onClick={() => selectCard()}>
-                <section
-                    className={"flex flex-col justify-center rounded border-2 p-6 shadow-xl duration-500 motion-safe:hover:scale-105 "
-                        + borderColor}>
-                    <h2 className="text-lg text-gray-700">{name}</h2>
-                    <p className="text-sm text-gray-600">{description}</p>
-                </section>
-            </button>
-        )
-            ;
+            <div>
+                <button onClick={() => selectCard()}>
+                    <section
+                        className={"flex flex-col justify-center rounded border-2 p-6 shadow-xl duration-500 motion-safe:hover:scale-105 "
+                            + borderColor}>
+                        <h2 className="text-lg text-gray-700">{name}</h2>
+                        <p className="text-sm text-gray-600">{description}</p>
+                    </section>
+                </button>
+            </div>
+        );
     }
 ;
 
-const UserCard = (
-    {
-        name, image, studyDate
-    }
-        : { name?: string, image?: string, studyDate?: string }) => {
+const UserCard = ({name, image, studyDate}: { name?: string, image?: string, studyDate?: string }) => {
     const borderColor = studyDate === "SATURDAY" ? "border-purple-500" : "border-indigo-500";
     return (
         <section
@@ -124,6 +127,25 @@ const UserCard = (
     );
 }
 
+const ErrorCard = ({setError}: { setError: (isError: boolean) => void }) => {
+    return (
+        <div id="alert-2" className="flex absolute text-sm top-5 right-4 p-4 bg-red-100 rounded-lg dark:bg-red-200"
+             role="alert">
+            <span className="font-medium pr-2">Sign-in please!</span>
+            <button type="button"
+                    onClick={() => setError(false)}
+                    className="ml-auto -mx-1.5 -my-1.5 bg-red-100 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 dark:bg-red-200 dark:text-red-600 dark:hover:bg-red-300"
+                    data-dismiss-target="#alert-2" aria-label="Close">
+                <span className="sr-only">Close</span>
+                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"></path>
+                </svg>
+            </button>
+        </div>)
+}
 
 const AuthShowcase: React.FC = () => {
         const {data: sessionData} = useSession();
